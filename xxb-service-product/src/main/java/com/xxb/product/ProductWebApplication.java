062ad7a41bdffbc889ef02ea35cbcb3e315e22b2
axj <s1249280492@163.com>
2023-03-07 18:22:32 +0800

    first commit

---
+++ xxb-service-product/src/main/java/com/xxb/product/ProductWebApplication.java
@@ -0,0 +1,18 @@
+package com.xxb.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 啊璇君
+ * @Title: 个人开发者-我的解放日志
+ * @Description:
+ * @date 2022/9/14 20:21
+ */
+@SpringBootApplication
+public class ProductWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductWebApplication.class,args);
+    }
+}
---
+++ xxb-service-user/src/main/java/com/xxb/user/UserWebApplication.java
@@ -0,0 +1,19 @@
+package com.xxb.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 啊璇君
+ * @Title: 个人开发者-我的解放日志
+ * @Description:
+ * @date 2022/9/14 20:27
+ */
+@SpringBootApplication
+public class UserWebApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(UserWebApplication.class,args);
+        }
+
+}
